fd95e4980d7bfa43d40c160daf4b4dd76a4c194e
Howly <artur.krol96@gmail.com>
2022-07-11 17:25:16 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿
+namespace Panel_logistyczny
+{
+    partial class Logowanie
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LogInBTN = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.LoginTB = new System.Windows.Forms.TextBox();
+            this.PassTB = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // LogInBTN
+            // 
+            this.LogInBTN.Location = new System.Drawing.Point(139, 149);
+            this.LogInBTN.Name = "LogInBTN";
+            this.LogInBTN.Size = new System.Drawing.Size(115, 41);
+            this.LogInBTN.TabIndex = 0;
+            this.LogInBTN.Text = "Zaloguj";
+            this.LogInBTN.UseVisualStyleBackColor = true;
+            this.LogInBTN.Click += new System.EventHandler(this.LogIn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(76, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Login:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(76, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Hasło:";
+            // 
+            // LoginTB
+            // 
+            this.LoginTB.Location = new System.Drawing.Point(135, 43);
+            this.LoginTB.Name = "LoginTB";
+            this.LoginTB.Size = new System.Drawing.Size(177, 23);
+            this.LoginTB.TabIndex = 3;
+            // 
+            // PassTB
+            // 
+            this.PassTB.Location = new System.Drawing.Point(135, 86);
+            this.PassTB.Name = "PassTB";
+            this.PassTB.PasswordChar = '*';
+            this.PassTB.Size = new System.Drawing.Size(177, 23);
+            this.PassTB.TabIndex = 4;
+            // 
+            // Logowanie
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 211);
+            this.Controls.Add(this.PassTB);
+            this.Controls.Add(this.LoginTB);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.LogInBTN);
+            this.Name = "Logowanie";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Logowanie";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button LogInBTN;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox LoginTB;
+        private System.Windows.Forms.TextBox PassTB;
+    }
+}
+
